b222bb9daf90fca1c8fe9d29e6589eb7f5d626ff
ardabuglagil <arda.buglagil@fau.de>
2023-11-20 20:11:30 +0100

    add faster jacobian computation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
-from sympy_demo import jacobian
+import scipy.linalg
+
+from sympy_demo import Jacobian_h, jacobian_numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from scipy.spatial.transform import Rotation as R
@@ -15,67 +17,13 @@ def mod_2pi(x):
     return mod
 
 
-def measure(antenna_positions, beacon_pos, phi_mix):
+def measure(antenna_positions, beacon_pos):
+    # random phase offset for receiver
+    phi_mix = np.random.rand() * 2 * np.pi
+
     tau = np.linalg.norm(antenna_positions - beacon_pos, axis=0) / c
     phi = mod_2pi(-2 * np.pi * f * tau + phi_mix).reshape((-1, 1))
     return phi
-
-
-ant_pos = np.array([[0, 0, 0], [2 * lmb, 0, 0], [0, 0, -2 * lmb], [5 * lmb, 0, 0], [0, 0, -5 * lmb], [10 * lmb, 0, 0],
-                    [0, 0, -10 * lmb], [10 * lmb, 0, -10 * lmb], [-10 * lmb, 0, 0], [-8 * lmb, 0, -5 * lmb]]).T
-i_list = [3]
-
-beacon_pos = np.array(
-    [
-        [0, 0, 0],
-        [0, 0, 0.05],
-        [0, 0, 0.1],
-        [0, 0, 0.15],
-        [0, 0, 0.2],
-        [0, 0, 0.25],
-        [0, 0, 0.30],
-        [0, 0, 0.35],
-        [0, 0, 0.4],
-        [0, 0, 0.45],
-        [0, 0, 0.50],
-        [0, 0, 0.55],
-        [0, 0, 0.6],
-        [0, 0, 0.65],
-        [0, 0, 0.70],
-        [0, 0, 0.75],
-        [0, 0, 0.80],
-        [0, 0, 0.85],
-        [0, 0, 0.90],
-        [0, 0, 0.95],
-        [0, 0, 1],
-        [0, 0.1, 1],
-        [0, 0.2, 1],
-        [0, 0.3, 1],
-        [0, 0.4, 1],
-        [0, 0.6, 1],
-        [0, 0.7, 1],
-        [0, 0.8, 1],
-        [0, 0.9, 1],
-        [0, 1, 1]
-    ]
-).T
-
-r1 = R.from_euler('xyz', [-45, 0, 0], degrees=True)
-R1 = r1.as_matrix()
-t1 = np.array([[0, -1, 2]]).T
-ant1_pos = R1 @ ant_pos + t1
-
-r2 = R.from_euler('xyz', [-45, 0, 90], degrees=True)
-R2 = r2.as_matrix()
-t2 = np.array([[1, 0, 2]]).T
-ant2_pos = R2 @ ant_pos + t2
-
-r3 = R.from_euler('xyz', [-45, 0, -135], degrees=True)
-R3 = r3.as_matrix()
-t3 = np.array([[-1, 1, 2]]).T
-ant3_pos = R3 @ ant_pos + t3
-
-print(measure(ant_pos, beacon_pos[:, 0].reshape((-1, 1)), 0))
 
 
 def compute_G(dt):
@@ -108,22 +56,90 @@ def compute_F(dt):
     return F
 
 
-xs = []
+def get_A_full(antenna_pos):
+    i_max = len(antenna_pos[0]) - 1
+    A_full = np.zeros((i_max, len(antenna_pos[0])))
+
+    for i in range(i_max):
+        A_full[i, 0] = 1
+        A_full[i, i + 1] = -1
+
+    return A_full
+
+
+ant_pos = np.array([[0, 0, 0], [2 * lmb, 0, 0], [0, 0, -2 * lmb], [5 * lmb, 0, 0], [0, 0, -5 * lmb], [10 * lmb, 0, 0],
+                    [0, 0, -10 * lmb], [10 * lmb, 0, -10 * lmb], [-10 * lmb, 0, 0], [-8 * lmb, 0, -5 * lmb]]).T
+i_list = [3, 3]
+
+beacon_pos = np.array(
+    [
+        [0, 0, 0],
+        [0, 0, 0.05],
+        [0, 0, 0.1],
+        [0, 0, 0.15],
+        [0, 0, 0.2],
+        [0, 0, 0.25],
+        [0, 0, 0.30],
+        [0, 0, 0.35],
+        [0, 0, 0.4],
+        [0, 0, 0.45],
+        [0, 0, 0.50],
+        [0, 0, 0.55],
+        [0, 0, 0.6],
+        [0, 0, 0.65],
+        [0, 0, 0.70],
+        [0, 0, 0.75],
+        [0, 0, 0.80],
+        [0, 0, 0.85],
+        [0, 0, 0.90],
+        [0, 0, 0.95],
+        [0, 0, 1],
+        [0, 0.1, 1],
+        [0, 0.2, 1],
+        [0, 0.3, 1],
+        [0, 0.4, 1],
+        [0, 0.5, 1],
+        [0, 0.6, 1],
+        [0, 0.7, 1],
+        [0, 0.8, 1],
+        [0, 0.9, 1],
+        [0, 1, 1],
+        [0, 1, 0.9],
+        [0, 1, 0.8],
+        [0, 1, 0.7],
+        [0, 1, 0.6],
+        [0, 1, 0.5]
+    ]
+).T
+
+r1 = R.from_euler('xyz', [-45, 0, 0], degrees=True)
+R1 = r1.as_matrix()
+t1 = np.array([[0, -1, 2]]).T
+ant1_pos = R1 @ ant_pos + t1
+
+r2 = R.from_euler('xyz', [-45, 0, 90], degrees=True)
+R2 = r2.as_matrix()
+t2 = np.array([[1, 0, 2]]).T
+ant2_pos = R2 @ ant_pos + t2
+
+r3 = R.from_euler('xyz', [-45, 0, -135], degrees=True)
+R3 = r3.as_matrix()
+t3 = np.array([[-1, 1, 2]]).T
+ant3_pos = R3 @ ant_pos + t3
+
+print(measure(ant_pos, beacon_pos[:, 0].reshape((-1, 1))))
+
+antenna_position_list = [ant1_pos, ant2_pos, ant3_pos]
 
 # initial state
+xs = []
 x = np.array([[0, 0, 0, 0, 0, 0]]).T
-dt = 1
-sigma = np.eye(len(x))
-
-i_max = len(ant_pos[0]) - 1
-A_full = np.zeros((i_max, len(ant_pos[0])))
-
-for i in range(i_max):
-    A_full[i, 0] = 1
-    A_full[i, i + 1] = -1
-
+dt = 0.1
+sigma = np.eye(len(x))*10
 sigma_phi = 0.00001
-sigma_a = 0.05
+sigma_a = 0.9
+A_full = get_A_full(ant_pos)
+jacobian_cache = {}
 
 for k in range(len(beacon_pos[0])):
     # prediction
@@ -136,32 +152,55 @@ for k in range(len(beacon_pos[0])):
     # iteration
     x_0 = x
     for i in i_list:
-        ant_pos_i = ant1_pos[:, : i]
-        phi = measure(ant_pos_i, beacon_pos[:, k].reshape((-1, 1)), 0)
+        ant_pos_i = []
+        A = []
+        R = []
+        h = []
+        phi = []
+        for ant_pos in antenna_position_list:
+            ant_pos_m_i = ant_pos[:, : i]
+            ant_pos_i.append(ant_pos_m_i)
 
-        A = A_full[: i - 1, : i]
+            phi_m = measure(ant_pos_m_i, beacon_pos[:, k].reshape((-1, 1)))
+            phi.append(phi_m)
+
+            A_m = A_full[: i - 1, : i]
+            A.append(A_m.tolist())
+
+            R_m = sigma_phi ** 2 * A_m @ A_m.T
+            R.append(R_m.tolist())
+
+            h_m = measure(ant_pos_m_i, x[:3].reshape((-1, 1)))
+            h.append(h_m)
+
+        ant_pos_i = np.hstack(ant_pos_i)
+        phi = np.vstack(phi)
+        A = scipy.linalg.block_diag(*A)
+        R = scipy.linalg.block_diag(*R)
+        h = np.vstack(h)
 
         z = A @ phi
+        h = mod_2pi(A @ h)
 
-        R = sigma_phi ** 2 * A @ A.T
-
-        H = jacobian(
-            A,
-            px=x[0, 0],
-            py=x[1, 0],
-            pz=x[2, 0],
-            w=2 * np.pi * f,
-            c=c,
-            ant_pos=ant_pos_i
-        )
+        H = jacobian_numpy(A_np=A, px=x[0, 0], py=x[1, 0], pz=x[2, 0], ant_pos=ant_pos_i, c=c, w0=2 * np.pi * f)
+        # if i not in jacobian_cache:
+        #
+        #     # use scipy implementation of jacobian (slow)
+        #     # h_jacobian = Jacobian_h(N=A.shape[0], I=A.shape[1], w0=2 * np.pi * f, c=c)
+        #     # jacobian_cache[i] = h_jacobian
+        #     #
+        #     # h_jacobian.compute_jacobian()
+        #     # H = h_jacobian.evaluate_jacobian(A_np=A, px=x[0, 0], py=x[1, 0], pz=x[2, 0], ant_pos=ant_pos_i)
+        #
+        #     # use numpy implementation of jacobian
+        #     H = jacobian_numpy(A_np=A, px=x[0, 0], py=x[1, 0], pz=x[2, 0], ant_pos=ant_pos_i, c=c, w0=2 * np.pi * f)
+        #     print("a")
+        # else:
+        #     H = jacobian_cache[i].evaluate_jacobian(A_np=A, px=x[0, 0], py=x[1, 0], pz=x[2, 0], ant_pos=ant_pos_i)
 
         K = sigma @ H.T @ np.linalg.inv(R + H @ sigma @ H.T)
 
-        h = measure(ant_pos_i, x[:3].reshape((-1, 1)), 0)
-        h = mod_2pi(A @ h)
-
-        kmk = K @ (mod_2pi(z - h) - H @ (x_0 - x))
-        x = x + kmk
+        x = x + K @ (mod_2pi(z - h) - H @ (x_0 - x))
 
     # update
     sigma = (np.eye(len(x)) - K @ H) @ sigma
@@ -184,7 +223,7 @@ ax.set_ylabel("y(m)")
 ax.set_zlabel("z(m)")
 
 # ax.plot3D(xs[:, 0], xs[:, 1], xs[:, 2], 'red')
-ax.scatter3D(xs[:, 0], xs[:, 1], xs[:, 2], c=xs[:, 2], cmap='cividis')
+ax.scatter3D(xs[:, 0], xs[:, 1], xs[:, 2], c='magenta')
 #
 ax.plot3D(beacon_pos[0, :], beacon_pos[1, :], beacon_pos[2, :], "green")
 ax.scatter3D(ant1_pos[0, :], ant1_pos[1, :], ant1_pos[2, :], c="red")
--- sympy_demo.py
+++ sympy_demo.py
@@ -3,49 +3,64 @@ import sympy as s
 import numpy as np
 
 
-def jacobian(A_np, px, py, pz, w, c, ant_pos):
-    N, I = A_np.shape
-    
-    x, y, z, w0, vx, vy, vz = s.symbols('x, y, z, w0, vx, vy, vz')
+class Jacobian_h():
+    def __init__(self, N, I, w0, c):
+        self.w0 = w0
+        self.c = c
+        self.N = N
+        self.I = I
+        self.H = None
 
-    pB = s.Matrix( [x, y, z ] )
-    A = MatrixSymbol('A', N, I)
-    phi_mix = MatrixSymbol("phi_mix", I, 1)
+    def compute_jacobian(self):
 
-    pMs = []
+        self.x, self.y, self.z, self.vx, self.vy, self.vz = s.symbols('x, y, z, vx, vy, vz')
 
-    for i in range(I):
-        pM = MatrixSymbol(f"pM^{i}", 3, 1)
-        pMs.append(pM)
+        pB = s.Matrix([self.x, self.y, self.z])
+        self.A = MatrixSymbol('A', self.N, self.I)
+        phi_mix = MatrixSymbol("phi_mix", self.I, 1)
 
-        tau_row = s.sqrt( (pB - pM).T * (pB - pM) ) / c
-        tau_row = s.Matrix(tau_row)
-        if i == 0:
-            tau = tau_row
-        else:
-            tau = tau.row_insert(i, tau_row)
+        self.pMs = []
+
+        for i in range(self.I):
+            pM = MatrixSymbol(f"pM^{i}", 3, 1)
+            self.pMs.append(pM)
+
+            tau_row = s.sqrt((pB - pM).T * (pB - pM)) / self.c
+            tau_row = s.Matrix(tau_row)
+            if i == 0:
+                tau = tau_row
+            else:
+                tau = tau.row_insert(i, tau_row)
+
+        phi = - self.w0 * tau + phi_mix
+        h = self.A * phi
+
+        h = h.as_explicit()
+        self.H = h.jacobian(Matrix([self.x, self.y, self.z, self.vx, self.vy, self.vz]))
+
+    def evaluate_jacobian(self, A_np, px, py, pz, ant_pos):
+
+        subs = {self.A: s.Matrix(A_np),
+                self.x: px,
+                self.y: py,
+                self.z: pz}
+
+        for i, pM in enumerate(self.pMs):
+            subs[pM] = s.Matrix(ant_pos[:, i])
+
+        F = np.array(self.H.evalf(subs=subs)).astype(float)
+        return F
 
 
 
-    phi = - w0 * tau + phi_mix
-    h = A * phi
-    h = Matrix(h)
+def jacobian_numpy(A_np, px, py, pz, ant_pos, c, w0):
+    p = np.array([[px, py, pz]]).T
+    u = p - ant_pos
+    u_norm = np.linalg.norm(u, axis=0)
+    k = u / u_norm
 
-    H = h.jacobian(Matrix([x, y, z, vx, vy, vz]))
-  
+    dxdydz = -w0 / c * A_np @ k.T
 
-
-    subs = {A: s.Matrix(A_np), 
-            x: px,
-            y: py,
-            z: pz,
-            w0: w
-            }
-
-
-    for i, pM in enumerate(pMs):
-        subs[pM] = s.Matrix(ant_pos[:, i])
-
-
-    F = np.array(H.evalf(subs=subs)).astype(float)
-    return F
+    jacobian = np.zeros((dxdydz.shape[0], 6)) # append dvx dvy dvz
+    jacobian[:, :3] = dxdydz
+    return jacobian
