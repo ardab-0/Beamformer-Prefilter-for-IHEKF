d45d925528d8103c596a1da575d2135ef5f12598
ardabuglagil <arda.buglagil@fau.de>
2023-11-21 16:28:04 +0100

    add spiral path

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ f = 24e9
 c = 2.998e8
 lmb = c / f
 
-
 def mod_2pi(x):
     mod = np.mod(x, 2 * np.pi)
     mod[mod >= np.pi] -= 2 * np.pi
     return mod
 
 
-def measure(antenna_positions, beacon_pos):
+def measure(antenna_positions, beacon_pos, sigma_phi):
     # random phase offset for receiver
     phi_mix = np.random.rand() * 2 * np.pi
-
+    #phi_mix = 0
     tau = np.linalg.norm(antenna_positions - beacon_pos, axis=0) / c
-    phi = mod_2pi(-2 * np.pi * f * tau + phi_mix).reshape((-1, 1))
+
+    n = np.random.randn(*tau.shape) * sigma_phi
+    phi = mod_2pi(-2 * np.pi * f * tau + phi_mix + n).reshape((-1, 1))
     return phi
 
 
@@ -61,84 +62,54 @@ def get_A_full(antenna_pos):
     A_full = np.zeros((i_max, len(antenna_pos[0])))
 
     for i in range(i_max):
-        A_full[i, 0] = 1
+        A_full[i, i] = 1
         A_full[i, i + 1] = -1
 
     return A_full
 
+def generate_spiral_path(a, theta_extent, alpha):
+    theta = np.linspace(0, theta_extent)
+    scaling = np.linspace(0.5, 1)
+
+    x = a*np.cos(theta) * scaling
+    y = a*np.sin(theta) * scaling
+    z = a*theta*np.tan(alpha)
+    return np.array([x, y, z]).reshape((3, -1))
+
 
 ant_pos = np.array([[0, 0, 0], [2 * lmb, 0, 0], [0, 0, -2 * lmb], [5 * lmb, 0, 0], [0, 0, -5 * lmb], [10 * lmb, 0, 0],
                     [0, 0, -10 * lmb], [10 * lmb, 0, -10 * lmb], [-10 * lmb, 0, 0], [-8 * lmb, 0, -5 * lmb]]).T
-i_list = [3, 3]
 
-beacon_pos = np.array(
-    [
-        [0, 0, 0],
-        [0, 0, 0.05],
-        [0, 0, 0.1],
-        [0, 0, 0.15],
-        [0, 0, 0.2],
-        [0, 0, 0.25],
-        [0, 0, 0.30],
-        [0, 0, 0.35],
-        [0, 0, 0.4],
-        [0, 0, 0.45],
-        [0, 0, 0.50],
-        [0, 0, 0.55],
-        [0, 0, 0.6],
-        [0, 0, 0.65],
-        [0, 0, 0.70],
-        [0, 0, 0.75],
-        [0, 0, 0.80],
-        [0, 0, 0.85],
-        [0, 0, 0.90],
-        [0, 0, 0.95],
-        [0, 0, 1],
-        [0, 0.1, 1],
-        [0, 0.2, 1],
-        [0, 0.3, 1],
-        [0, 0.4, 1],
-        [0, 0.5, 1],
-        [0, 0.6, 1],
-        [0, 0.7, 1],
-        [0, 0.8, 1],
-        [0, 0.9, 1],
-        [0, 1, 1],
-        [0, 1, 0.9],
-        [0, 1, 0.8],
-        [0, 1, 0.7],
-        [0, 1, 0.6],
-        [0, 1, 0.5]
-    ]
-).T
 
-r1 = R.from_euler('xyz', [-45, 0, 0], degrees=True)
+beacon_pos = generate_spiral_path(a=1, theta_extent=20, alpha=np.pi/45)
+
+r1 = R.from_euler('xyz', [-45, 0, -30], degrees=True)
 R1 = r1.as_matrix()
-t1 = np.array([[0, -1, 2]]).T
+t1 = np.array([[-1, -1.5, 3]]).T
 ant1_pos = R1 @ ant_pos + t1
 
-r2 = R.from_euler('xyz', [-45, 0, 90], degrees=True)
+r2 = R.from_euler('xyz', [-50, 0, 90], degrees=True)
 R2 = r2.as_matrix()
-t2 = np.array([[1, 0, 2]]).T
+t2 = np.array([[1.5, 0, 3]]).T
 ant2_pos = R2 @ ant_pos + t2
 
-r3 = R.from_euler('xyz', [-45, 0, -135], degrees=True)
+r3 = R.from_euler('xyz', [-45, 0, -160], degrees=True)
 R3 = r3.as_matrix()
-t3 = np.array([[-1, 1, 2]]).T
+t3 = np.array([[-0.5, 1.5, 3]]).T
 ant3_pos = R3 @ ant_pos + t3
 
-print(measure(ant_pos, beacon_pos[:, 0].reshape((-1, 1))))
 
 antenna_position_list = [ant1_pos, ant2_pos, ant3_pos]
 
 # initial state
 xs = []
-x = np.array([[0, 0, 0, 0, 0, 0]]).T
+x = np.array([[beacon_pos[0, 0], beacon_pos[1, 0], beacon_pos[2, 0], 0, 0, 0]]).T
 dt = 0.1
-sigma = np.eye(len(x))*10
-sigma_phi = 0.00001
-sigma_a = 0.9
+sigma = np.eye(len(x))
+sigma_phi = 0.01
+sigma_a = 0.1
 A_full = get_A_full(ant_pos)
+i_list = [3, 5]
 jacobian_cache = {}
 
 for k in range(len(beacon_pos[0])):
@@ -161,7 +132,7 @@ for k in range(len(beacon_pos[0])):
             ant_pos_m_i = ant_pos[:, : i]
             ant_pos_i.append(ant_pos_m_i)
 
-            phi_m = measure(ant_pos_m_i, beacon_pos[:, k].reshape((-1, 1)))
+            phi_m = measure(ant_pos_m_i, beacon_pos[:, k].reshape((-1, 1)), sigma_phi=sigma_phi)
             phi.append(phi_m)
 
             A_m = A_full[: i - 1, : i]
@@ -170,7 +141,7 @@ for k in range(len(beacon_pos[0])):
             R_m = sigma_phi ** 2 * A_m @ A_m.T
             R.append(R_m.tolist())
 
-            h_m = measure(ant_pos_m_i, x[:3].reshape((-1, 1)))
+            h_m = measure(ant_pos_m_i, x[:3].reshape((-1, 1)), sigma_phi=0)
             h.append(h_m)
 
         ant_pos_i = np.hstack(ant_pos_i)
@@ -200,7 +171,7 @@ for k in range(len(beacon_pos[0])):
 
         K = sigma @ H.T @ np.linalg.inv(R + H @ sigma @ H.T)
 
-        x = x + K @ (mod_2pi(z - h) - H @ (x_0 - x))
+        x = x + K @ (mod_2pi(z - h) - H @ (x_0 - x)) ################################################## in paper: x_0 - x
 
     # update
     sigma = (np.eye(len(x)) - K @ H) @ sigma
@@ -216,7 +187,7 @@ fig = plt.figure()
 ax = plt.axes(projection="3d")
 ax.set_xlim([-1.5, 1.5])
 ax.set_ylim([-1.5, 1.5])
-ax.set_zlim([-1.5, 1.5])
+ax.set_zlim([-0.5, 2.5])
 
 ax.set_xlabel("x(m)")
 ax.set_ylabel("y(m)")
@@ -229,5 +200,6 @@ ax.plot3D(beacon_pos[0, :], beacon_pos[1, :], beacon_pos[2, :], "green")
 ax.scatter3D(ant1_pos[0, :], ant1_pos[1, :], ant1_pos[2, :], c="red")
 ax.scatter3D(ant2_pos[0, :], ant2_pos[1, :], ant2_pos[2, :], c="blue")
 ax.scatter3D(ant3_pos[0, :], ant3_pos[1, :], ant3_pos[2, :], c="cyan")
+ax.scatter3D(0, 0, 0, c="black")
 
 plt.show()
