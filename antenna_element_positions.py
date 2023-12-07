152477a80ba3f35887cb9b63837be118a48866ac
ardabuglagil <arda.buglagil@fau.de>
2023-12-07 22:31:08 +0300

    add new configurations

--- antenna_element_positions.py
+++ antenna_element_positions.py
@@ -22,5 +22,54 @@ def generate_antenna_element_positions(kind: str, lmb: float) -> np.ndarray:
              [3 * lmb / 2, 0, -2 * lmb / 2],
              [0, 0, -3*lmb / 2], [12*lmb , 0, -3 * lmb / 2], [24 * lmb / 2, 0, -3 * lmb / 2],
              [48 * lmb / 2, 0, -3 * lmb / 2]]).T
+    elif kind == "regular_8_2":
+        return np.array(
+            [[0, 0, 0], [lmb / 2, 0, 0],
+             [0, 0, lmb / 2], [lmb / 2, 0, lmb / 2],
+             [0, 0, 2*lmb / 2], [lmb / 2, 0, 2*lmb / 2],
+             [0, 0, 3*lmb / 2], [lmb / 2, 0, 3*lmb / 2],
+             [0, 0, 4*lmb / 2], [lmb / 2, 0, 4*lmb / 2],
+             [0, 0, 5*lmb / 2], [lmb / 2, 0, 5*lmb / 2],
+             [0, 0, 6*lmb / 2], [lmb / 2, 0, 6*lmb / 2],
+             [0, 0, 7*lmb / 2], [lmb / 2, 0, 7*lmb / 2]
+             ]).T
+    elif kind == "regular_16_1":
+        return np.array(
+            [[0, 0, 0],
+             [0, 0, lmb / 2],
+             [0, 0, 2*lmb / 2],
+             [0, 0, 3*lmb / 2],
+             [0, 0, 4*lmb / 2],
+             [0, 0, 5*lmb / 2],
+             [0, 0, 6*lmb / 2],
+             [0, 0, 7*lmb / 2],
+             [0, 0, 8 * lmb / 2],
+             [0, 0, 9 * lmb / 2],
+             [0, 0, 10 * lmb / 2],
+             [0, 0, 11 * lmb / 2],
+             [0, 0, 12 * lmb / 2],
+             [0, 0, 13 * lmb / 2],
+             [0, 0, 14 * lmb / 2],
+             [0, 0, 15 * lmb / 2]
+             ]).T
+    elif kind == "irregular_16_1":
+        return np.array(
+            [[0, 0, 0],
+             [0, 0, lmb / 2],
+             [0, 0, 2*lmb / 2],
+             [0, 0, 3*lmb / 2],
+             [0, 0, 4*lmb / 2],
+             [0, 0, 5*lmb / 2],
+             [0, 0, 6*lmb / 2],
+             [0, 0, 7*lmb / 2],
+             [0, 0, 9 * lmb / 2],
+             [0, 0, 11 * lmb / 2],
+             [0, 0, 13 * lmb / 2],
+             [0, 0, 15 * lmb / 2],
+             [0, 0, 17 * lmb / 2],
+             [0, 0, 19 * lmb / 2],
+             [0, 0, 21 * lmb / 2],
+             [0, 0, 23 * lmb / 2]
+             ]).T
     else:
         raise ValueError("Wrong antenna kind.")
