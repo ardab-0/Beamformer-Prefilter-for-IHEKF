a93619c4e9d2e29de55d9dc2101c7d73048b70c6
ardabuglagil <arda.buglagil@fau.de>
2023-12-13 18:13:53 +0300

    move general functions to utils

--- utils.py
+++ utils.py
@@ -24,3 +24,22 @@ def find_minima(data:np.ndarray)->np.ndarray:
     res = np.array(np.where(1 == minima)).T
     return res
 
+
+def generate_spiral_path(a, theta_extent, alpha):
+    theta = np.linspace(0, theta_extent)
+    scaling = np.linspace(0.5, 1)
+
+    x = a * np.cos(theta) * scaling
+    y = a * np.sin(theta) * scaling
+    z = a * theta * np.tan(alpha)
+    return np.array([x, y, z]).reshape((3, -1))
+
+def mse(x, x_hat):
+    return np.mean(np.square(x - x_hat))
+
+
+
+def mod_2pi(x):
+    mod = np.mod(x, 2 * np.pi)
+    mod[mod >= np.pi] -= 2 * np.pi
+    return mod
