a7e1ecb0a19b9b704f58ad1be354b86a81c9613b
ardabuglagil <arda.buglagil@fau.de>
2023-12-18 20:32:24 +0300

    add beamformer generator

---
+++ beamformer/beamformer.py
@@ -0,0 +1,21 @@
+from beamformer.fourier import FourierBeamformer
+from beamformer.capon import CaponBeamformer
+from beamformer.music import MusicBeamformer
+
+default_configuration = {"fourier": {"compute_method": "gpu"},
+                             "capon": {"compute_method": "cpu"},
+                             "music": {"compute_method": "cpu"}}
+def generate_beamformer(beamformer_type: str, config: dict = None):
+    if config is None:
+        compute_method = default_configuration[beamformer_type]["compute_method"]
+    else:
+        compute_method = config[beamformer_type]["compute_method"]
+
+    if beamformer_type == "fourier":
+        return FourierBeamformer(type=compute_method)
+    elif beamformer_type == "capon":
+        return CaponBeamformer(type=compute_method)
+    elif beamformer_type == "music":
+        return MusicBeamformer(type=compute_method)
+    else:
+        raise ValueError("Wrong beamformer type.")
