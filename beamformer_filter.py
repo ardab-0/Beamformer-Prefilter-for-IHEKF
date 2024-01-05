ed0d0c054bd9c0b2b37f96d3e9b1ee30e7b6d7d9
ardabuglagil <arda.buglagil@fau.de>
2024-01-05 21:49:10 +0300

    add recording of phase differences

--- beamformer_filter.py
+++ beamformer_filter.py
@@ -9,7 +9,14 @@ from beamformer.beamformer import generate_beamformer
 import measurement_simulation as sim
 import spatial_filter
 
-antenna_element_positions, A_full = generate_antenna_element_positions(kind="square_8_8", lmb=params.lmb,
+#################### params
+visualize = False
+
+################### params
+
+
+
+antenna_element_positions, A_full = generate_antenna_element_positions(kind="square_4_4", lmb=params.lmb,
                                                                        get_A_full=True)
 antenna_element_positions[[0, 1], :] = antenna_element_positions[[1, 0], :]  # switch x and y rows
 
@@ -30,33 +37,47 @@ multipath_sources = [{"x": 0,
 
 # multipath_sources = []
 
+recorded_phi_differences = []
+recorded_phi_no_multipath_differences = []
+recorded_phi_filtered_differences = []
 for k in range(len(beacon_pos[0])):
     phi_B = np.random.rand() * 2 * np.pi  # transmitter phase at time k
 
-    for antenna in antenna_list:
-        ant_pos = antenna.get_antenna_positions()
+    antenna = antenna_list[0]
+    ant_pos = antenna.get_antenna_positions()
 
-        target_dir = beacon_pos[:3, k].reshape((-1, 1)) - antenna.get_t()
-        target_dir_r, target_dir_theta, target_dir_phi = utils.cartesian_to_spherical(target_dir[0], target_dir[1], target_dir[2])
-        print(f"Target direction theta: {target_dir_theta}, phi: {target_dir_phi}")
-        for multipath in multipath_sources:
-            dir = np.array([multipath["x"], multipath["y"], multipath["z"]]).reshape((-1, 1)) - antenna.get_t()
-            m_r, m_t, m_p = utils.cartesian_to_spherical(dir[0], dir[1], dir[2])
-            print(f"Multipath direction theta: {m_t}, phi: {m_p}")
+    target_dir = beacon_pos[:3, k].reshape((-1, 1)) - antenna.get_t()
+    target_dir_r, target_dir_theta, target_dir_phi = utils.cartesian_to_spherical(target_dir[0], target_dir[1],
+                                                                                  target_dir[2])
+    print(f"Target direction theta: {target_dir_theta}, phi: {target_dir_phi}")
+    for multipath in multipath_sources:
+        dir = np.array([multipath["x"], multipath["y"], multipath["z"]]).reshape((-1, 1)) - antenna.get_t()
+        m_r, m_t, m_p = utils.cartesian_to_spherical(dir[0], dir[1], dir[2])
+        print(f"Multipath direction theta: {m_t}, phi: {m_p}")
 
-        s_m = sim.measure_s_m_multipath(t=t, antenna_positions=ant_pos,
-                                        beacon_pos=beacon_pos[:, k].reshape((-1, 1)),
-                                        phi_B=phi_B, sigma=0.01, multipath_sources=multipath_sources)
+    s_m = sim.measure_s_m_multipath(t=t, antenna_positions=ant_pos,
+                                    beacon_pos=beacon_pos[:, k].reshape((-1, 1)),
+                                    phi_B=phi_B, sigma=0.00001, multipath_sources=multipath_sources)
 
+    s_m_no_multipath = sim.measure_s_m_multipath(t=t, antenna_positions=ant_pos,
+                                    beacon_pos=beacon_pos[:, k].reshape((-1, 1)),
+                                    phi_B=phi_B, sigma=0.00001, multipath_sources=[])
+
+    phi = sim.measure_phi(s_m=s_m, f_m=params.f, t=t)
+    phi_no_multipath = sim.measure_phi(s_m=s_m_no_multipath, f_m=params.f, t=t)
+    recorded_phi_differences.append(utils.mod_2pi(A_full @ phi))
+    recorded_phi_no_multipath_differences.append(utils.mod_2pi(A_full @ phi_no_multipath))
+
+    if visualize:
         results, output_signals, thetas, phis = beamformer.compute_beampattern(x=s_m,
                                                                                N_theta=params.N_theta,
                                                                                N_phi=params.N_phi,
                                                                                fs=params.fs,
                                                                                r=ant_pos)
 
-
         element_beampattern, theta_e, phi_e = antenna.get_antenna_element_beampattern(thetas=thetas,
                                                                                       phis=phis)
+
 
         fig = plt.figure()
         ax = plt.axes(projection="3d")
@@ -88,17 +109,21 @@ for k in range(len(beacon_pos[0])):
         fig.colorbar(surf, shrink=0.5, aspect=5)
 
 
+    s_m_filtered = spatial_filter.iterative_max_2D_filter(x=s_m,
+                                                          r=ant_pos,
+                                                          beamformer=beamformer,
+                                                          antenna=antenna,
+                                                          peak_threshold=0.01,
+    target_theta=target_dir_theta,
+    target_phi=target_dir_phi,
+    d_theta=np.deg2rad(params.target_theta_range_deg),
+    d_phi=np.deg2rad(params.target_phi_range_deg))
+
+    phi_filtered = sim.measure_phi(s_m=s_m_filtered, f_m=params.f, t=t)
+    recorded_phi_filtered_differences.append(utils.mod_2pi(A_full @ phi_filtered))
 
 
-        s_m_filtered = spatial_filter.iterative_max_2D_filter(x=s_m,
-                                                              r=ant_pos,
-                                                              beamformer=beamformer,
-                                                              antenna=antenna,
-                                                              target_theta=target_dir_theta,
-                                                              target_phi=target_dir_phi,
-                                                              d_theta=np.deg2rad(params.target_theta_range_deg),
-                                                              d_phi=np.deg2rad(params.target_phi_range_deg))
-
+    if visualize:
         results, output_signals, thetas, phis = beamformer.compute_beampattern(x=s_m_filtered,
                                                                                N_theta=params.N_theta,
                                                                                N_phi=params.N_phi,
@@ -140,6 +165,19 @@ for k in range(len(beacon_pos[0])):
             plt.subplot(r, r, i + 1)
             plt.plot((s_m[i, :]), label="s_m")
             plt.plot((s_m_filtered[i, :]), label="s_m filtered")
+            plt.plot((s_m_no_multipath[i, :]), label="s_m no multipath")
             plt.legend()
 
         plt.show()
+
+
+recorded_phi_differences = np.asarray(recorded_phi_differences)
+recorded_phi_differences = recorded_phi_differences.squeeze()
+recorded_phi_no_multipath_differences = np.asarray(recorded_phi_no_multipath_differences)
+recorded_phi_no_multipath_differences = recorded_phi_no_multipath_differences.squeeze()
+recorded_phi_filtered_differences = np.asarray(recorded_phi_filtered_differences)
+recorded_phi_filtered_differences = recorded_phi_filtered_differences.squeeze()
+
+np.save("phase_difference_measurements/phi_differences", recorded_phi_differences)
+np.save("phase_difference_measurements/phi_no_multipath_differences", recorded_phi_no_multipath_differences)
+np.save("phase_difference_measurements/phi_filtered_differences", recorded_phi_filtered_differences)
