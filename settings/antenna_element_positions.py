96663b4f8872ac3334cdbc91119e4ad76c00b7bf
ardabuglagil <arda.buglagil@fau.de>
2024-01-09 16:15:21 +0300

    add 6 by 6 antenna configuartion

--- settings/antenna_element_positions.py
+++ settings/antenna_element_positions.py
@@ -130,6 +130,15 @@ def generate_antenna_element_positions(kind: str, lmb: float, get_A_full: bool=F
         A_full = sim.get_A_full(antenna_element_positions)
 
         return antenna_element_positions, A_full
+    elif kind == "square_6_6":
+        t = np.arange(0, 6)* lmb/2
+        x, y = np.meshgrid(t, t)
+
+        antenna_element_positions = np.stack([x, np.zeros_like(x), -y]).reshape((3, -1))
+
+        A_full = sim.get_A_full(antenna_element_positions)
+
+        return antenna_element_positions, A_full
 
     elif kind == "square_8_8":
         t = np.arange(0, 8)* lmb/2
