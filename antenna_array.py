2247e92ee2494efa624996217f97f2774a9f8d4b
ardabuglagil <arda.buglagil@fau.de>
2024-02-20 20:24:31 +0100

    add antenna array interface

--- antenna_array.py
+++ antenna_array.py
@@ -4,10 +4,25 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 import utils
+from settings.antenna_element_positions import generate_antenna_element_positions
 from utils import cartesian_to_spherical
 
 
 class AntennaArray:
+    def get_antenna_positions(self):
+        raise NotImplementedError
+
+    def get_R(self):
+        raise NotImplementedError
+
+    def get_t(self):
+        raise NotImplementedError
+
+    def get_antenna_element_beampattern(self, thetas, phis):
+        raise NotImplementedError
+
+
+class SimulationAntennaArray(AntennaArray):
     def __init__(self, rot: list, t: list, element_positions: np.ndarray):
         self._rot_list = rot
         self._R = self._compute_R(rot)
@@ -45,18 +60,43 @@ class AntennaArray:
         return self.antenna_element.get_element_pattern(thetas, phis)
 
 
-class RealDataAntennaArray:
-    def __init__(self):
-        self.antenna_positions = None
+class RealDataAntennaArray(AntennaArray):
+    def __init__(self, params, antenna_element_pos):
+        self.antenna_element_positions = antenna_element_pos
+        self.params = params
+        self.model_antenna_element_positions, _ = generate_antenna_element_positions(kind=params.antenna_kind,
+                                                                                     lmb=params.lmb,
+                                                                                     get_A_full=True)
+        self._R, self._t = self._compute_R_t()
+        theta, phi = self._compute_theta_phi()
+        self.antenna_element = AntennaElement(mu_theta=theta, mu_phi=phi, sigma_phi=1, sigma_theta=1)
 
-    def load_antenna_positions(self, antenna_pos):
-        self.antenna_positions = antenna_pos
+    def _compute_R_t(self):
+        return utils.optimal_rotation_and_translation(self.model_antenna_element_positions,
+                                                      self.antenna_element_positions)
 
     def get_antenna_positions(self):
-        return self.antenna_positions
+        return self.antenna_element_positions
+
+    def _compute_theta_phi(self):
+        initial_direction = np.array([[1, 0, 0]]).T
+
+        new_direction = self._R @ initial_direction
+        r, theta, phi = cartesian_to_spherical(new_direction[0], new_direction[1], new_direction[2])
+        return theta, phi
+
+    # def get_t(self):
+    #     return np.mean(self.antenna_positions, axis=1).reshape((3, 1))
+
+    def get_R(self):
+        return self._R
 
     def get_t(self):
-        return np.mean(self.antenna_positions, axis=1).reshape((3, 1))
+        return self._t
+
+    def get_antenna_element_beampattern(self, thetas, phis):
+        return self.antenna_element.get_element_pattern(thetas, phis)
+
 
 class AntennaElement:
     def __init__(self, mu_theta=0, mu_phi=0, sigma_theta=0.3, sigma_phi=0.3):
@@ -68,11 +108,10 @@ class AntennaElement:
     def get_element_pattern(self, thetas, phis):
         theta, phi = np.meshgrid(thetas, phis)
         shape = theta.shape
-        dir = np.array([np.sin(self.mu_theta) * np.cos(self.mu_phi), np.sin(self.mu_theta) * np.sin(self.mu_phi), np.cos(self.mu_theta)])
-
+        dir = np.array([np.sin(self.mu_theta) * np.cos(self.mu_phi), np.sin(self.mu_theta) * np.sin(self.mu_phi),
+                        np.cos(self.mu_theta)])
 
         pattern = np.ones_like(theta)
-
 
         # t_i = np.logical_and(theta > theta_range[0], theta < theta_range[1])
         # p_i = np.logical_and(phi > phi_range[0], phi < phi_range[1])
@@ -88,15 +127,11 @@ class AntennaElement:
         projection = dir @ cartesian
         pattern_1[projection < 0] = 0
 
-
         # rotated_spherical = utils.cartesian_to_spherical_np(rotated_cartesian[0], rotated_cartesian[1], rotated_cartesian[2])
 
         pattern_1 = pattern_1.reshape(shape)
 
-
-
         return pattern_1, theta, phi
-
 
 # phis = np.linspace(-1 * np.pi, np.pi, 100)
 # thetas = np.linspace(0, np.pi, 100)
